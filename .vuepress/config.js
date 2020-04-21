8d8cb66274acf24e6a5775342dee8082a41a72f9
agrahamg <andrew.grahamg@gmail.com>
2020-04-21 02:05:36 +0000

    Deploying to gh-pages from  @ 18651f6a5104fc5acd16de53b43d1634088ac47d 🚀

---
+++ .vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  themeConfig: {
+    nav: [
+      {
+        text: 'Home', link: '/',
+      }, {
+        text: 'Guide', link: '/guide/gettingStarted',
+      }, {
+        text: 'Examples', link: '/examples/examples'
+      }
+    ],
+    sidebar: [
+      '/',
+      '/guide/gettingStarted',
+      '/guide/reference',
+      '/examples/examples'
+    ]
+  }
+}
